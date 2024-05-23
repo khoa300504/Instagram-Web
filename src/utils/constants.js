12f1b6e577be6226e4d85c193cc94a0a909bd63b
khoa300504 <khoa300504@gmail.com>
2024-05-23 13:35:13 +0700

    Bug Deploy#1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
-const apiRoot = 'http://localhost:8017'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev')
+{
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production')
+{
+  apiRoot = 'https://instagram-api-vgh8.onrender.com'
+}
+
 export const API_ROOT = apiRoot
